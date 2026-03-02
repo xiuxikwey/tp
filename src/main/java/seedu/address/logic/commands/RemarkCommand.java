5eaf4a0598d02a109cac6e91fa38525759823506
xiuxi <177907531+xiuxikwey@users.noreply.github.com>
2026-03-02 16:13:21 +0800

    Remove binary files

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
