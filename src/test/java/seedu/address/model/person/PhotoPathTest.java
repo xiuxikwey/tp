464d85b001af05fc4af51c9ccbacb3daab682cf4
Jotham Lim <EnderSky@users.noreply.github.com>
2026-03-26 00:22:03 +0800

    Remove unused imports

--- src/test/java/seedu/address/model/person/PhotoPathTest.java
+++ src/test/java/seedu/address/model/person/PhotoPathTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
